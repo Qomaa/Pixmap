14b9985e80e253f8aabc74b1d7a909e5fa95a570
Andreas Hoffmann <->
2017-12-01 16:34:25 +0100

    Loading map "inital"

--- public/javascripts/Map.js
+++ public/javascripts/Map.js
@@ -5,7 +5,7 @@ var Map = (function () {
     }
     Map.prototype.Save = function () {
         var mapString = JSON.stringify(this, function (key, value) {
-            if (key == "DivElement")
+            if (key === "DivElement")
                 return undefined;
             return value;
         });
@@ -19,39 +19,38 @@ var Map = (function () {
         var m;
         var self = this;
         //Initial
-        //this.Width = 400;
-        //this.Height = 400;
-        //self.DivElement.style.width = this.Width + "px";
-        //self.DivElement.style.height = this.Height + "px";
-        //self.RowCount = 50;
-        //self.ColumnCount = 50;
-        //let fieldWidth: number = this.Width / self.RowCount;
-        //let fieldHeight: number = this.Height / self.ColumnCount;
-        //for (var x = 0; x < self.RowCount; x++) {
-        //    for (var y = 0; y < self.ColumnCount; y++) {
-        //        let mapField = new MapField(x, y, fieldWidth, fieldHeight, "yellow");
-        //        self.MapFields.push(mapField);
-        //        self.DivElement.appendChild(mapField.GetDivElement());
-        //    }
-        //}
+        this.Width = 400;
+        this.Height = 400;
+        self.DivElement.style.width = this.Width + "px";
+        self.DivElement.style.height = this.Height + "px";
+        self.RowCount = 50;
+        self.ColumnCount = 50;
+        var fieldWidth = this.Width / self.RowCount;
+        var fieldHeight = this.Height / self.ColumnCount;
+        for (var x = 0; x < self.RowCount; x++) {
+            for (var y = 0; y < self.ColumnCount; y++) {
+                var mapField = new MapField(x, y, fieldWidth, fieldHeight, "yellow");
+                self.MapFields.push(mapField);
+                self.DivElement.appendChild(mapField.GetDivElement());
+            }
+        }
         //From Server server holen
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", "/mapload", true);
-        xhttp.setRequestHeader("Content-Type", "application/json" /*"text/plain"*/);
-        xhttp.onreadystatechange = function () {
-            if (xhttp.readyState !== XMLHttpRequest.DONE || xhttp.status !== 200)
-                return;
-            m = JSON.parse(xhttp.responseText);
-            self.Width = m.Width;
-            self.Height = m.Height;
-            self.DivElement.style.width = m.Width + "px";
-            self.DivElement.style.height = m.Height + "px";
-            self.RowCount = m.RowCount;
-            self.ColumnCount = m.ColumnCount;
-            self.MapFields = m.MapFields;
-            self.GenerateMap();
-        };
-        xhttp.send();
+        //let xhttp: XMLHttpRequest = new XMLHttpRequest();
+        //xhttp.open("GET", "/mapload", true);
+        //xhttp.setRequestHeader("Content-Type", "application/json" /*"text/plain"*/)
+        //xhttp.onreadystatechange = function () {
+        //    if (xhttp.readyState !== XMLHttpRequest.DONE || xhttp.status !== 200) return;
+        //    m = JSON.parse(xhttp.responseText);
+        //    self.Width = m.Width;
+        //    self.Height = m.Height;
+        //    self.DivElement.style.width = m.Width + "px";
+        //    self.DivElement.style.height = m.Height + "px";
+        //    self.RowCount = m.RowCount;
+        //    self.ColumnCount = m.ColumnCount;
+        //    self.MapFields = m.MapFields;
+        //    self.GenerateMap();
+        //}
+        //xhttp.send();
     };
     Map.prototype.GenerateMap = function () {
         var self = this;
--- public/javascripts/Map.ts
+++ public/javascripts/Map.ts
@@ -3,7 +3,7 @@
 
     Save() {
         let mapString = JSON.stringify(this, function (key: string, value: any) {
-            if (key == "DivElement") return undefined;
+            if (key === "DivElement") return undefined;
             return value;
         });
 
@@ -20,41 +20,41 @@
         let self = this;
 
         //Initial
-        //this.Width = 400;
-        //this.Height = 400;
+        this.Width = 400;
+        this.Height = 400;
 
-        //self.DivElement.style.width = this.Width + "px";
-        //self.DivElement.style.height = this.Height + "px";
-        //self.RowCount = 50;
-        //self.ColumnCount = 50;
-        //let fieldWidth: number = this.Width / self.RowCount;
-        //let fieldHeight: number = this.Height / self.ColumnCount;
-        //for (var x = 0; x < self.RowCount; x++) {
-        //    for (var y = 0; y < self.ColumnCount; y++) {
-        //        let mapField = new MapField(x, y, fieldWidth, fieldHeight, "yellow");
-        //        self.MapFields.push(mapField);
-        //        self.DivElement.appendChild(mapField.GetDivElement());
-        //    }
-        //}
+        self.DivElement.style.width = this.Width + "px";
+        self.DivElement.style.height = this.Height + "px";
+        self.RowCount = 50;
+        self.ColumnCount = 50;
+        let fieldWidth: number = this.Width / self.RowCount;
+        let fieldHeight: number = this.Height / self.ColumnCount;
+        for (var x = 0; x < self.RowCount; x++) {
+            for (var y = 0; y < self.ColumnCount; y++) {
+                let mapField = new MapField(x, y, fieldWidth, fieldHeight, "yellow");
+                self.MapFields.push(mapField);
+                self.DivElement.appendChild(mapField.GetDivElement());
+            }
+        }
 
         //From Server server holen
-        let xhttp: XMLHttpRequest = new XMLHttpRequest();
-        xhttp.open("GET", "/mapload", true);
-        xhttp.setRequestHeader("Content-Type", "application/json" /*"text/plain"*/)
-        xhttp.onreadystatechange = function () {
-            if (xhttp.readyState !== XMLHttpRequest.DONE || xhttp.status !== 200) return;
+        //let xhttp: XMLHttpRequest = new XMLHttpRequest();
+        //xhttp.open("GET", "/mapload", true);
+        //xhttp.setRequestHeader("Content-Type", "application/json" /*"text/plain"*/)
+        //xhttp.onreadystatechange = function () {
+        //    if (xhttp.readyState !== XMLHttpRequest.DONE || xhttp.status !== 200) return;
 
-            m = JSON.parse(xhttp.responseText);
-            self.Width = m.Width;
-            self.Height = m.Height;
-            self.DivElement.style.width = m.Width + "px";
-            self.DivElement.style.height = m.Height + "px";
-            self.RowCount = m.RowCount;
-            self.ColumnCount = m.ColumnCount;
-            self.MapFields = m.MapFields;
-            self.GenerateMap();
-        }
-        xhttp.send();
+        //    m = JSON.parse(xhttp.responseText);
+        //    self.Width = m.Width;
+        //    self.Height = m.Height;
+        //    self.DivElement.style.width = m.Width + "px";
+        //    self.DivElement.style.height = m.Height + "px";
+        //    self.RowCount = m.RowCount;
+        //    self.ColumnCount = m.ColumnCount;
+        //    self.MapFields = m.MapFields;
+        //    self.GenerateMap();
+        //}
+        //xhttp.send();
     }
 
     GenerateMap() {
