6223688f36b6b706d29a61db86b2643a601357a0
Andreas Hoffmann <->
2017-12-16 11:22:21 +0100

    port:80, metatags

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.use(function (err, req, res, next) {
         error: {}
     });
 });
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 var server = app.listen(app.get('port'), function () {
     debug('Express server listening on port ' + server.address().port);
 });
