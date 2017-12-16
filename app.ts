5a665a3728c31064aee9afff1478389827ad016b
Andreas Hoffmann <->
2017-12-16 15:15:47 +0100

    delete debug msg

--- app.ts
+++ app.ts
@@ -27,7 +27,6 @@ app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
 app.get("/address", function (request, response) {
     response.send(process.env.IOTA_ADDRESS);
-    console.log(process.env.IOTA_ADDRESS);
 });
 
 app.get("/mapload", function (request, response) {
